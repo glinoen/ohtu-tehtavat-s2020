01342b99d47d48854fdf219fbd9e4ad660a8f313
glinoen <juho.linden@gmail.com>
2020-11-05 18:03:46 +0200

    injections with spring

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -0,0 +1,47 @@
+package ohtu.verkkokauppa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Kauppa {
+
+    private StorageIO varasto;
+    private BankIO pankki;
+    private Ostoskori ostoskori;
+    private GeneratorIO viitegeneraattori;
+    private String kaupanTili;
+
+    @Autowired
+    public Kauppa(Varasto storage, Pankki bank, Viitegeneraattori generator) {
+        varasto = storage;
+        pankki = bank;
+        viitegeneraattori = generator;
+        kaupanTili = "33333-44455";
+    }
+
+    public void aloitaAsiointi() {
+        ostoskori = new Ostoskori();
+    }
+
+    public void poistaKorista(int id) {
+        Tuote t = varasto.haeTuote(id); 
+        varasto.palautaVarastoon(t);
+    }
+
+    public void lisaaKoriin(int id) {
+        if (varasto.saldo(id)>0) {
+            Tuote t = varasto.haeTuote(id);             
+            ostoskori.lisaa(t);
+            varasto.otaVarastosta(t);
+        }
+    }
+
+    public boolean tilimaksu(String nimi, String tiliNumero) {
+        int viite = viitegeneraattori.uusi();
+        int summa = ostoskori.hinta();
+        
+        return pankki.tilisiirto(nimi, viite, tiliNumero, kaupanTili, summa);
+    }
+
+}
